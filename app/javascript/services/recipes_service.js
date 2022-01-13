e539885ebeb2c43f8088f70e349d119b18220b9c
AlvaroJTorres <mangetrone@gmail.com>
2022-01-12 20:38:48 -0500

    Added service to call the rails controller to get a specific recipe

--- app/javascript/services/recipes_service.js
+++ app/javascript/services/recipes_service.js
@@ -1,3 +1,7 @@
 export function getRecipes(query) {
   return fetch(`/recipes?search=${query}`).then((res) => res.json());
 }
+
+export function getRecipe(id) {
+  return fetch(`/recipes/${id}`).then((res) => res.json());
+}
