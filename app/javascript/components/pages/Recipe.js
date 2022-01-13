8d22549ba5960f7abe4da58ba761d0193c14a1d4
AlvaroJTorres <mangetrone@gmail.com>
2022-01-12 20:39:26 -0500

    Updated recipe page component to call the recipes_service and both get and display the recipe data

--- app/javascript/components/pages/Recipe.js
+++ app/javascript/components/pages/Recipe.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getRecipe } from "../../services/recipes_service";
 
 export default function Recipe() {
   let params = useParams();
-  return <div>{params.id}</div>;
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    const showRecipe = async () => {
+      const recipeData = await getRecipe(params.id);
+      if (recipeData.status === "error") {
+        alert(recipeData.message);
+      } else {
+        setRecipe(recipeData.data.recipe);
+      }
+    };
+    showRecipe();
+  }, []);
+  return <div>{recipe.title}</div>;
 }
