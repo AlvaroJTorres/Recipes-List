699e447a8bdd027b765cb4f42b0417bb5e5a2d39
AlvaroJTorres <mangetrone@gmail.com>
2022-01-12 19:41:04 -0500

    Created a Home Page that displays the Search bar and the results from the API after the search is fullfiled

---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import useSearchRecipes from "../../hooks/useSearchRecipes";
+import Search from "../Search";
+
+export default function Home() {
+  const [query, setQuery, recipes, loading, error] = useSearchRecipes();
+  return (
+    <>
+      <Search
+        query={query}
+        setQuery={setQuery}
+        loading={loading}
+        error={error}
+      />
+      <div>
+        {recipes &&
+          recipes.map((recipe) => (
+            <Link key={recipe.id} to={`recipe/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          ))}
+      </div>
+    </>
+  );
+}
