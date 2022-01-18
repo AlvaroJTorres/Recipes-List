bf2cab8716056b67125fbd148885b943410b4494
AlvaroJTorres <mangetrone@gmail.com>
2022-01-18 12:52:42 -0500

    Added service to create a new recipe and updated the services with the new routes

--- app/javascript/services/recipes_service.js
+++ app/javascript/services/recipes_service.js
@@ -1,7 +1,17 @@
 export function getRecipes(query) {
-  return fetch(`/recipes?search=${query}`).then((res) => res.json());
+  return fetch(`api/v1/recipes?search=${query}`).then((res) => res.json());
 }
 
 export function getRecipe(id) {
-  return fetch(`/recipes/${id}`).then((res) => res.json());
+  return fetch(`api/v1/recipes/${id}`).then((res) => res.json());
+}
+
+export function createRecipe(data) {
+  return fetch("api/v1/recipes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 }
