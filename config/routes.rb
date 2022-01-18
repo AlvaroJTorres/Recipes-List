6002b29f3cc4d80ae0d3f66e2534809a9d0efaf4
AlvaroJTorres <mangetrone@gmail.com>
2022-01-18 12:51:16 -0500

    Updated routes according to the new controller distribution

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'recipes#index'
 
-  resources :recipes
+  get '/:id', to: 'recipes#show'
+  get '/', to: 'recipes#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
